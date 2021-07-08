3c472d7ef8f33791d0b9de5271d9f97e3f9a94d6
Sudharshana-V <65701112+Sudharshana-V@users.noreply.github.com>
2021-07-08 21:04:09 +0530

    Add files via upload

---
+++ MemberMicroservice/src/main/java/com/cognizant/membermicroservice/MemberMicroserviceApplication.java
@@ -0,0 +1,19 @@
+package com.cognizant.membermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+
+@SpringBootApplication
+@EnableSwagger2
+public class MemberMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberMicroserviceApplication.class, args);
+		
+	}
+
+}
