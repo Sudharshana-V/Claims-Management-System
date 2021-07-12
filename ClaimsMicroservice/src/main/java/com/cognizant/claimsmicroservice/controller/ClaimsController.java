3c6e37a2b16cba85d4ed162c0b332340da6b2d0f
Sudharshana-V <65701112+Sudharshana-V@users.noreply.github.com>
2021-07-12 22:42:17 +0530

    Add files via upload

---
+++ ClaimsMicroservice/src/main/java/com/cognizant/claimsmicroservice/controller/ClaimsController.java
@@ -0,0 +1,86 @@
+package com.cognizant.claimsmicroservice.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.claimsmicroservice.client.PolicyClient;
+import com.cognizant.claimsmicroservice.dto.ClaimStatusDTO;
+import com.cognizant.claimsmicroservice.exception.BenefitsNotFoundException;
+import com.cognizant.claimsmicroservice.exception.ProviderNotFoundException;
+
+import com.cognizant.claimsmicroservice.repository.ClaimRepository;
+
+import com.cognizant.claimsmicroservice.service.ClaimServiceImpl;
+
+
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@RestController
+public class ClaimsController {
+
+	@Autowired
+	ClaimServiceImpl claimserviceImpl;
+
+
+	@Autowired
+	ClaimStatusDTO claimsDto;
+
+	@Autowired
+	ClaimRepository claimrepo;
+	
+	@Autowired
+	PolicyClient policyClient;
+	
+	
+	@GetMapping(value = "/getClaimStatus/claimid/{claimid}/policyid/{policyid}/memberid/{memberid}")
+	public ClaimStatusDTO getClaimStatus(@PathVariable int claimid,@PathVariable int policyid,@PathVariable int memberid) throws ProviderNotFoundException
+	{
+				//return claimrepo.findByclaimId(claimid);
+				return claimserviceImpl.getClaimStatus(claimid, policyid, memberid);
+		
+	}
+	
+		/*return new ResponseEntity<ClaimStatusDTO>(claimserviceImpl.getClaimStatus(claimId, policyId, memberId),
+				HttpStatus.OK);*/
+	
+	
+	
+
+	/*@PostMapping(value = "/submitClaim/{policyId}/{claimId}/{memberId}/{providerId}/{benefitId}/{totalAmount}/{claimedAmount}")
+	public ClaimStatusDTO submitClaim(@PathVariable Integer policyid, @PathVariable Integer claimid,
+			@PathVariable Integer memberid, @PathVariable Integer providerid, @PathVariable Integer benefitid,
+			@PathVariable Double totalamount, @PathVariable Double claimedamount) throws ProviderNotFoundException, BenefitsNotFoundException {
+		
+		
+		return ClaimStatusDTO(claimsDto);
+	}*/
+	
+	@PostMapping(value = "/submitClaim/{policyId}/{claimId}/{memberId}/{providerId}/{benefitId}/{totalAmount}/{claimedAmount}")
+	public ClaimStatusDTO submitClaim(@PathVariable int policyId, @PathVariable int claimId,
+			@PathVariable int memberId, @PathVariable int providerId, @PathVariable int benefitId,
+			@PathVariable double totalAmount, @PathVariable double claimedAmount) throws ProviderNotFoundException, BenefitsNotFoundException {
+		
+			
+			claimsDto = claimserviceImpl.processSubmitClaim(policyId, claimId, memberId, providerId, benefitId, totalAmount,
+					claimedAmount);
+
+		
+		return claimsDto;
+	}
+
+	
+}
+
+
